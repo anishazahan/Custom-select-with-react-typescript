2d9c8bab25dfaf1b337f76d3980d716ae4edf54f
anishazahan <anishazahan13@gmail.com>
2023-04-20 12:57:47 +0600

    suceesfully get for selecting options

--- src/Select.tsx
+++ src/Select.tsx
@@ -13,19 +13,39 @@ type selectProps = {
 
 const Select = ({options,value,onChange}:selectProps) => {
     const [isOpen,setIsOpen] = useState(false)
+
+        function clearOption(e:any) {
+            e.stopPropagation()
+            onChange(undefined)
+        }
+        function selectOption(option:selectOption) {
+            onChange(option)
+        }
+
+
   return (
    <>
 
-        <div onClick={()=>setIsOpen(prev=>!prev)} tabIndex={0} className={styles.container}>
+        <div 
+        onClick={()=>setIsOpen(prev=>!prev)}
+        onBlur={() => setIsOpen(false)}
+         tabIndex={0} className={styles.container}>
 
             <span className={styles.value}>{value?.label}</span>
-            <button className={styles['clear-btn']}>&times;</button>
+            <button onClick={e=>clearOption(e)} className={styles['clear-btn']}>&times;</button>
             <div className={styles.divider}></div>
             <div className={styles.caret}></div>
             <ul className={`${styles.options} ${isOpen ?styles.show : ""}`}>
                 {
+                    
                     options.map((option,index)=>{
-                     return <li className={styles.option} key={index}>{option.label}</li>
+                     return <li
+                     onClick={e => {
+                        e.stopPropagation()
+                        selectOption(option)
+                        setIsOpen(false)
+                      }}
+                     className={styles.option} key={index}>{option.label}</li>
                     })
                 }
             </ul>
